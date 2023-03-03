e5889bf71c0e77e62e5ddb5ca10440f61a4ae2c8
aleme <aleme@DESKTOP-5T9QAJU>
2023-03-02 23:28:47 -0600

    Agregar archivos de proyecto.

---
+++ WebApplicationTarea2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationTarea2.Global" Language="C#" %>
---
+++ WebApplicationTarea2/Models/puesto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace WebApplicationTarea2.Models
+{
+    public class puesto
+    {
+        [ScaffoldColumn(false)]
+        public int id_edificio { get; set; }
+
+        [Required, StringLength(150), Display(Name = "Name")]
+        public string nombre_edificio { get; set; }
+    }
+}
